2133ec73332d9a9c33c14e2f73fa62133be83ef4
mono-glyceride <glyceride.mono@gmail.com>
2022-01-17 14:29:58 +0900

    binding

--- app/src/main/java/com/example/gurise/outofbussinesscards/MainActivity.kt
+++ app/src/main/java/com/example/gurise/outofbussinesscards/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gurise.outofbussinesscards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gurise.outofbussinesscards.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
     }
 }
