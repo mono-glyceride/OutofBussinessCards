8fb77874371bfd22e29ee03bd5ddc23d052aa277
mono-glyceride <glyceride.mono@gmail.com>
2022-01-18 17:39:39 +0900

    main_activity

--- app/src/main/java/com/example/gurise/outofbussinesscards/MainActivity.kt
+++ app/src/main/java/com/example/gurise/outofbussinesscards/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.gurise.outofbussinesscards
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import com.example.gurise.outofbussinesscards.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).apply {
+            val company = getString("company","")
+            val postal = getString("postal","")
+            val address = getString("address","")
+            val tel = getString("tel","")
+            val fax = getString("fax","")
+            val email = getString("email","")
+            val url = getString("url","")
+            val position = getString("position","")
+            val name = getString("name","")
+
+            binding.companyText.text = company
+            binding.postalText.text = postal
+            binding.addressText.text = address
+            binding.telText.text = tel
+            binding.faxText.text = fax
+            binding.emailText.text = email
+            binding.urlText.text = url
+            binding.positionText.text = position
+            binding.nameText.text = name
+        }
     }
 }
